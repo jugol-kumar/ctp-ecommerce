028203fed96af7b2df5f0ab9180523d0f25897be
jugol-kumar <jk23717933@gmail.com>
2023-02-18 16:28:47 +0600

    add dashboard link in admin

--- resources/js/Shared/SideNav.vue
+++ resources/js/Shared/SideNav.vue
@@ -34,7 +34,7 @@
         <div class="main-menu-content">
             <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
                 <li class=" nav-item">
-                    <a class="d-flex align-items-center" href="index.html">
+                    <a class="d-flex align-items-center" :href="$page.props.auth.MAIN_URL">
                         <vue-feather type="home" />
                         <span class="menu-title text-truncate" data-i18n="Dashboards">Dashboards</span>
                     </a>
